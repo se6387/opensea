e0a36ceb3855788c30ac04c90a3ee374b19bc027
Ahadu Melesse <ahadu.melesse@gmail.com>
2022-09-30 11:22:46 -0500

    crawl an href using lxml

--- initial.py
+++ initial.py
@@ -1,13 +1,32 @@
 import requests
 import re
+import lxml.html
 
 def extractLinkRegEx(txt):
     tgs = re.compile(r'<a[^<>]+?href=([\'\"])(.*?)\1', re.IGNORECASE)
     return [match[1] for match in tgs.findall(txt)]
+
+# use lxml parser
+def extractLinkslLxml(text):
+    lists = []
+    dom = lxml.html.fromstring(text)
+    for list in dom.xpath('//a/@href'):
+        lists.append(list)
+    
+    return lists
+
+# print line by line
+def printList(lists):
+    for list in lists:
+        print ('Level 1 -> ' + list)
 
 # opensea.io
 # edfreitas.me
 # https://quotes.toscrape.com/
 
 r = requests.get('https://opensea.io') 
-print(extractLinkRegEx(r.text))
+printList(extractLinkRegEx(r.text))
+
+#
+print('')
+printList(extractLinkslLxml(r.text))
