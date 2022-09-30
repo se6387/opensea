4ab8cabcb01e0fa713d2171fb4d7619968cc89ab
Ahadu Melesse <ahadu.melesse@gmail.com>
2022-09-30 10:45:50 -0500

    fix regex error

--- initial.py
+++ initial.py
@@ -2,10 +2,11 @@ import requests
 import re
 
 def extractLinkRegEx(txt):
-    tgs = re.compile(r'<a[^<>]+?href=([\'\"])(.?*)\1', re.IGNORECASE)
+    tgs = re.compile(r'<a[^<>]+?href=([\'\"])(.*?)\1', re.IGNORECASE)
     return [match[1] for match in tgs.findall(txt)]
 
 # opensea.io
+# edfreitas.me
 # https://quotes.toscrape.com/
 
 r = requests.get('https://opensea.io') 
