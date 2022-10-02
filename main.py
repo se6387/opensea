5e79eab94879b44ca6221c08299cd91a5a96ffb0
Ahadu Melesse <ahadu.melesse@gmail.com>
2022-10-02 01:11:48 -0500

    update main to accomodate html parser and make code efficient

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import web_response
+import print_lists
 import extract_links_regex
 import extract_links_lxml
-import print_lists
-import web_response
+import html_parser
+
 
 def main():
     # webs to crawl ..
@@ -9,11 +11,23 @@ def main():
     # edfreitas.me
     # quotes.toscrape.com
 
-    response = web_response.getWebResponse('https://opensea.io')
+    webToCrawl = 'https://opensea.io'
+    response = web_response.getWebResponse(webToCrawl)
+    print('crawling : ', webToCrawl, ' ..', sep='')
+
+    # regex
+    print('')
+    print('using regex ..')
     print_lists.printLists(extract_links_regex.extractLinksRegEx(response.text))
 
-    #
+    # lxml
     print('')
+    print('using lxml ..')
     print_lists.printLists(extract_links_lxml.extractLinksLxml(response.text))
+
+    # html parser
+    print('')
+    print('using html ..')
+    print_lists.printLists(html_parser.extractLinksHtmlParser(response.text))
 
 main()
