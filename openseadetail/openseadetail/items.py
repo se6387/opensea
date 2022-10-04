62380d631eb522b26cda74a1e7073e67d9a03baa
Ahadu Melesse <ahadu.melesse@gmail.com>
2022-10-04 00:56:31 -0500

    add out of the the box scrapy crawler files for opensea io

---
+++ openseadetail/openseadetail/items.py
@@ -0,0 +1,17 @@
+import scrapy
+
+class OpenSeaDetailItem(scrapy.Item):
+    # define the items that will be extracted
+
+    #contact address & token id
+    cati = scrapy.Field()
+
+    # token standard, block chain, last updated, & creator earnings
+    tsbcluce = scrapy.Field()
+    
+    # contractAddress = scrapy.Field()
+    # tokenId = scrapy.Field()
+    # tokenStandard = scrapy.Field()
+    # blockChain = scrapy.Field()
+    # lastUpdated = scrapy.Field()
+    # creatorEarnings = scrapy.Field()
