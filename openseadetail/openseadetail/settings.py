b3cae618775b7cd39d252b9a21bff7c262381c97
Ahadu Melesse <ahadu.melesse@gmail.com>
2022-10-06 19:55:49 -0500

    add user agent - in settings

--- openseadetail/openseadetail/settings.py
+++ openseadetail/openseadetail/settings.py
@@ -15,6 +15,7 @@ NEWSPIDER_MODULE = 'openseadetail.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'openseadetail (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win 64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
