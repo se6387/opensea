7561d3a3030d21f4f3ba82f3372c66c85f4de37d
Ahadu Melesse <ahadu.melesse@gmail.com>
2022-10-14 01:06:46 -0500

    add a spider to crawl a specific page detail

---
+++ openseadetail/openseadetail/spiders/openseaspider.py
@@ -0,0 +1,21 @@
+import scrapy
+
+class OpenSeaSpider(scrapy.Spider):
+    name = 'openseaspider'
+    allowed_domains = [
+        'opensea.io'
+    ]
+    start_urls = [
+        'https://opensea.io/assets/ethereum/0xaea4fa9451f527d5f36e65f833d88dbb56a0c264/1831'
+    ]
+
+    def parse(self, response):
+        item = response.css('div.sc-29427738-0')
+        yield {
+            'contactAddress': item.css('span.sc-29427738-0').css('a.sc-1f719d57-0::text').extract()[0],
+            'tokenId': item.css('span.sc-29427738-0').css('a.sc-1f719d57-0::text').extract()[1],
+            'tokenStandard': item.css('span.sc-29427738-0::text').extract()[0],
+            'blockChain': item.css('span.sc-29427738-0::text').extract()[1],
+            'lastUpdated': item.css('span.sc-29427738-0::text').extract()[2],
+            'creatorEarnings': item.css('span.sc-29427738-0::text').extract()[3]
+        }
