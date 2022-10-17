2dd168503b538a83e10efc3630e280e584a033b9
Ahadu Melesse <ahadu.melesse@gmail.com>
2022-10-16 23:09:43 -0500

    update open sea spider: read start urls from file

--- openseadetail/openseadetail/spiders/openseaspider.py
+++ openseadetail/openseadetail/spiders/openseaspider.py
@@ -5,11 +5,13 @@ class OpenSeaSpider(scrapy.Spider):
     allowed_domains = [
         'opensea.io'
     ]
-    start_urls = [
-        'https://opensea.io/assets/ethereum/0x394e3d3044fc89fcdd966d3cb35ac0b32b0cda91/8175',
-        'https://opensea.io/assets/ethereum/0x394e3d3044fc89fcdd966d3cb35ac0b32b0cda91/5388',
-        'https://opensea.io/assets/ethereum/0xaea4fa9451f527d5f36e65f833d88dbb56a0c264/1831'
-    ]
+    # start_urls = [
+    #     'https://opensea.io/assets/ethereum/0x394e3d3044fc89fcdd966d3cb35ac0b32b0cda91/8175',
+    #     'https://opensea.io/assets/ethereum/0x394e3d3044fc89fcdd966d3cb35ac0b32b0cda91/5388',
+    #     'https://opensea.io/assets/ethereum/0xaea4fa9451f527d5f36e65f833d88dbb56a0c264/1831'
+    # ]
+    with open('starturls.txt', 'rt') as f:
+        start_urls = [url.strip() for url in f.readlines()]
 
     def parse(self, response):
         item = response.css('div.sc-29427738-0')
