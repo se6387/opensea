16401969b4a6d3e04d2186888c152eabd51205e8
Ahadu Melesse <ahadu.melesse@gmail.com>
2022-11-13 23:02:37 -0600

    add configuration to work with splash to settings

--- openseadetail/openseadetail/settings.py
+++ openseadetail/openseadetail/settings.py
@@ -12,10 +12,25 @@ BOT_NAME = 'openseadetail'
 SPIDER_MODULES = ['openseadetail.spiders']
 NEWSPIDER_MODULE = 'openseadetail.spiders'
 
-
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'openseadetail (+http://www.yourdomain.com)'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win 64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
+
+# Splash Setup
+SPLASH_URL = 'http://localhost:8050'
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+}
+
+SPIDER_MIDDLEWARES = {
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
+
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
