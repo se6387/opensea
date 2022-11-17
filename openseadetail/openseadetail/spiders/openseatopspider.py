8e7fb1449ff2200109c5fd5b6028c04918e70467
zach-neiger <znn180000@utdallas.edu>
2022-11-17 10:49:02 -0600

    Migrated some aspects of detail crawler into top spider. Currently testing effects.

--- openseadetail/openseadetail/spiders/openseatopspider.py
+++ openseadetail/openseadetail/spiders/openseatopspider.py
@@ -12,3 +12,49 @@ class OpenSeaTopSpider(scrapy.Spider):
 
     def __init__(self):
         self.deleteFileIfExists()
+
+    def parseurls(self, response):
+        openSeaDetailItem = OpenSeaDetailItem()
+        item = response.css('div.sc-29427738-0')
+
+        yield {
+            'LP': item.css('div.sc-29427738-0').css('div.sc-29427738-0::text').extract()
+        }
+
+        if item.css('div.sc-29427738-0').css('div.sc-29427738-0::text').extract()[8] == 'Last Updated':
+            global flag
+            flag = True
+            yield {
+                'Contract Address': item.css('span.sc-29427738-0').css('a.sc-1f719d57-0::text').extract()[0],
+                'Token ID': item.css('span.sc-29427738-0').css('a.sc-1f719d57-0::text').extract()[1],
+                'Token Standard': item.css('span.sc-29427738-0::text').extract()[0],
+                'Block Chain': item.css('span.sc-29427738-0::text').extract()[1],
+                'Last Updated': item.css('span.sc-29427738-0::text').extract()[2],
+                'Creator Earnings': item.css('span.sc-29427738-0::text').extract()[3]
+            }
+
+            openSeaDetailItem['contractAddress'] = item.css('span.sc-29427738-0').css('a.sc-1f719d57-0::text').extract()[0]
+            openSeaDetailItem['tokenID'] = item.css('span.sc-29427738-0').css('a.sc-1f719d57-0::text').extract()[1]
+            openSeaDetailItem['tokenStandard'] = item.css('span.sc-29427738-0::text').extract()[0]
+            openSeaDetailItem['chain'] = item.css('span.sc-29427738-0::text').extract()[1]
+            openSeaDetailItem['lastUpdated'] = item.css('span.sc-29427738-0::text').extract()[2]
+            openSeaDetailItem['creatorEarnings'] = item.css('span.sc-29427738-0::text').extract()[3]
+
+            self.writeToFile(openSeaDetailItem)
+        else:
+            flag = False
+
+            yield {
+                'Contract Address': item.css('span.sc-29427738-0').css('a.sc-1f719d57-0::text').extract()[0],
+                'Token ID': item.css('span.sc-29427738-0').css('a.sc-1f719d57-0::text').extract()[1],
+                'Token Standard': item.css('span.sc-29427738-0::text').extract()[0],
+                'Block Chain': item.css('span.sc-29427738-0::text').extract()[1],
+                'Creator Earnings': item.css('span.sc-29427738-0::text').extract()[2]
+            }
+            openSeaDetailItem['contractAddress'] = item.css('span.sc-29427738-0').css('a.sc-1f719d57-0::text').extract()[0]
+            openSeaDetailItem['tokenID'] = item.css('span.sc-29427738-0').css('a.sc-1f719d57-0::text').extract()[1]
+            openSeaDetailItem['tokenStandard'] = item.css('span.sc-29427738-0::text').extract()[0]
+            openSeaDetailItem['chain'] = item.css('span.sc-29427738-0::text').extract()[1]
+            openSeaDetailItem['creatorEarnings'] = item.css('span.sc-29427738-0::text').extract()[2]
+
+            self.writeToFile(openSeaDetailItem)
