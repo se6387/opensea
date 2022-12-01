eb8a67492c2e6e0dfa90eaa7f3565ba976f28c7a
Ahadu Melesse <ahadu.melesse@gmail.com>
2022-12-01 01:19:55 -0600

    add file writter to ether scan spider

--- openseadetail/openseadetail/spiders/etherscanspider.py
+++ openseadetail/openseadetail/spiders/etherscanspider.py
@@ -20,11 +20,23 @@ class EtherScanSpider(scrapy.Spider):
     ]
 
     def deleteFileIfExists(self):
-        if os.path.exists('openseaetherscan.txt'):
-            os.remove('openseaetherscan.txt')
+        if os.path.exists('etherscantransaction.txt'):
+            os.remove('etherscantransaction.txt')
 
     def __init__(self):
         self.deleteFileIfExists()
+
+    def writeToFile(self, openSeaDetailItem):
+        with codecs.open('etherscantransaction.txt', 'a+', 'utf-8') as f:
+            f.write('Transaction Hash : ' + openSeaDetailItem['transactionHash'] + '\r\n')
+            f.write('Status : ' + openSeaDetailItem['status'] + '\r\n')
+            f.write('Block : ' + openSeaDetailItem['block'] + '\r\n')
+            f.write('Timestamp : ' + openSeaDetailItem['timestamp'] + '\r\n')
+            f.write('From : ' + openSeaDetailItem['fromF'] + '\r\n')
+            f.write('To : ' + openSeaDetailItem['to'] + '\r\n')
+            f.write('Value : ' + openSeaDetailItem['value'] + '\r\n')
+            f.write('Transaction Fee : ' + openSeaDetailItem['transactionFee'] + '\r\n')
+            f.write('Gas Price : ' + openSeaDetailItem['gasPrice'] + '\r\n\n\n')
 
     def parseEtherScan(self, response):
         openSeaDetailItem = OpenSeaDetailItem()
@@ -42,6 +54,18 @@ class EtherScanSpider(scrapy.Spider):
             'Transaction Fee': response.css('div.col-md-9').css('span::text').extract()[9] + '.' + response.css('div.col-md-9').css('span::text').extract()[10],
             'Gas Price': response.css('div.col-md-9').css('span::text').extract()[11] + response.css('div.col-md-9').css('span::text').extract()[12] + response.css('div.col-md-9').css('span::text').extract()[13]
         }
+
+        openSeaDetailItem['transactionHash'] = response.css('div.col-md-9').css('span.mr-1::text').get()
+        openSeaDetailItem['status'] = response.css('div.col').css('span.u-label::text').get()
+        openSeaDetailItem['block'] = response.css('div.col-md-9').css('a::text').extract()[2]
+        openSeaDetailItem['timestamp'] = response.css('div.row').css('div.col-md-9::text').extract()[8]
+        openSeaDetailItem['fromF'] = response.css('div.col-md-9').css('a.mr-1::text').extract()[0]
+        openSeaDetailItem['to'] = response.css('div.col-md-9').css('a.mr-1::text').extract()[1]
+        openSeaDetailItem['value'] = response.css('div.col-md-9').css('span::text').extract()[7] + ' ' + response.css('div.col-md-9').css('span::text').extract()[8]
+        openSeaDetailItem['transactionFee'] = response.css('div.col-md-9').css('span::text').extract()[9] + '.' + response.css('div.col-md-9').css('span::text').extract()[10],
+        openSeaDetailItem['gasPrice'] = response.css('div.col-md-9').css('span::text').extract()[11] + response.css('div.col-md-9').css('span::text').extract()[12] + response.css('div.col-md-9').css('span::text').extract()[13]
+
+        
 
     def parse(self, response):
         for link in response.css('td').css('a.hash-tag::attr(href)'):
